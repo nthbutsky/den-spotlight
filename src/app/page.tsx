81c23ea51befdc2ae5e3c0245bfeddd1ad9357b7
nthbutsky <nthbutsky@gmail.com>
2023-09-25 13:43:00 -0700

    fix: links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -255,12 +255,12 @@ export default function Home() {
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/nthbutsky"
               aria-label="Follow on GitHub"
               icon={faGithub}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/denisbutsky"
               aria-label="Follow on LinkedIn"
               icon={faLinkedin}
             />

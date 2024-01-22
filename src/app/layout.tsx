9c643d312907745fd7d4d92c63e2f37d0767ee3f
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-01-22 17:56:53 +0200

    fix: adjust content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ import Analytics from '@/components/Analytics'
 export const metadata: Metadata = {
   title: {
     template: '%s - Denis Butsky',
-    default: 'Denis Butsky - Tech wizard, photographer, and Swiss Army Knife man.',
+    default: 'Denis Butsky - Tech wizard, photographer, and Swiss Army Knife.',
   },
   description:
-    'I’m Denis Butsky, a web developer and entrepreneur. I live in Vancouver BC, where I design the future.',
+    'I’m Denis Butsky, a web developer and problem-solver. I live in Vancouver BC, where I design the future.',
 }
 
 export default function RootLayout({

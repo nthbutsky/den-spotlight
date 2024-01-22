43d53a7f763bdc335b8a87b05a75ef0b3ee015a9
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2024-01-22 18:05:31 +0200

    fix: adjust content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import Analytics from '@/components/Analytics'
 export const metadata: Metadata = {
   title: {
     template: '%s - Denis Butsky',
-    default: 'Denis Butsky - Tech wizard, photographer, and Swiss Army Knife.',
+    default: 'Denis Butsky - Tech wizard, photographer, Swiss Army Knife.',
   },
   description:
     'I’m Denis Butsky, a web developer and problem-solver. I live in Vancouver BC, where I design the future.',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -296,7 +296,7 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Tech wizard, photographer, and Swiss Army Knife.
+            Tech wizard, photographer, Swiss Army Knife.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Denis Butsky, a web developer and problem-solver. I live in

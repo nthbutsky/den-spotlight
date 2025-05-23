2081bdf2236fe44964fe40bc51528af02479a034
Denis Butsky <30866948+nthbutsky@users.noreply.github.com>
2025-05-23 09:05:59 -0700

    fix: change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -298,7 +298,6 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            <TextGenerateEffect words="Swiss Army Knife." />
             <TextGenerateEffect words="Generalist. Explorer. Wizard." />
           </h1>
           <div className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
